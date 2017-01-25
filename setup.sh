b061e162f1b2627d8dd22cf7e235c268d9662955
Henry Paz <henry@Henrys-MacBook-Air.local>
2017-01-25 00:59:58 -0600

    upload setup.sh file to create reproducible environment

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # Create an environment and install dependencies
-# This first part of the anlaysis is to install the dependency 
-# software and associated packages in the hopes of creating a reproducible 
-# environment to work from over time. 
+# Install software and dependencies to create reproducible 
+# environment as the one used for this study
 
 # ensure pwd is the cloned repository
 result=${PWD##*/}
@@ -10,3 +9,51 @@ then
 	printf "\nCurrent directory is not the cloned repository.\nSee https://github.com/enriquepaz/rumen_microbiome_feed_efficiency_beef for details.\n\n"
 	exit 1
 fi
+
+# ensure provided link to usearch download
+if [ "$1" = "" ]; then
+    printf "\nProvide a link for USEARCH download (from email) as argument.\nGet a license from http://www.drive5.com/usearch/download.html\nSee https://github.com/enriquepaz/rumen_microbiome_feed_efficiency_beef for details.\n\n"
+    exit 1
+fi
+
+# anaconda 
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
+bash Anaconda-2.3.0-Linux-x86_64.sh
+anaconda/bin/conda create -n microbiomeBeef python=2.7 qiime=1.9.1
+source anaconda/bin/activate microbiomeBeef
+
+# r
+conda install -c r rpy2=2.5.6 r-devtools=1.9.1 r-curl=0.9.4 r-XML r-car 
+conda install -c r r=3.2.2
+
+# pandoc
+conda install -c https://conda.binstar.org/asmeurer pandoc
+
+# R packages
+printf "\nInstallling R packages, will take some time...\n"
+R CMD BATCH scripts/install_pack.R
+
+# mothur
+wget https://github.com/mothur/mothur/releases/download/v1.36.1/Mothur.mac_64.OSX-10.9.zip
+unzip Mothur.mac_64.OSX-10.9.zip
+mv mothur/mothur anaconda/envs/microbiomeBeef/bin/
+rm -r mothur
+rm -r __MACOSX
+
+# usearch
+wget -O anaconda/envs/microbiomeBeef/bin/usearch7.0.1090 $1
+chmod 775 anaconda/envs/microbiomeBeef/bin/usearch7.0.1090
+#mv usearch7.0.10 anaconda/envs/microbiomeBeef/bin/
+
+
+
+
+
+
+
+
+
+
+
+
+
