649e3e74f269f299fb1973a3ceeee9b4ef760234
Henry Paz <henry@Henrys-MacBook-Air.local>
2017-01-25 18:22:26 -0600

    working in setup.sh file

--- setup.sh
+++ setup.sh
@@ -17,7 +17,8 @@ if [ "$1" = "" ]; then
 fi
 
 # anaconda 
-wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
+#wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 bash Anaconda*.sh
 anaconda/bin/conda create -n microbiomeBeef python=2.7 qiime=1.9.1
 source anaconda/bin/activate microbiomeBeef
