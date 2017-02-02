0c85b0adc98b182db1d180c5a2a31d34e9736fe9
Henry Paz <henry@pcp270954pcs.unl.edu>
2017-02-01 23:34:47 -0600

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ fi
 cd ..
 wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
 bash Anaconda2-4.3.0-Linux-x86_64.sh
-anaconda2/bin/conda create -n microbiomeBeef python=2.7 qiime=1.9.1
-source anaconda2/bin/activate microbiomeBeef 
+anaconda/bin/conda create -n microbiomeBeef python=2.7 qiime=1.9.1
+source anaconda/bin/activate microbiomeBeef 
 #pip install --upgrade setuptools
 #pip install https://github.com/biocore/qiime/archive/1.9.1.tar.gz
 rm Anaconda2-4.3.0-Linux-x86_64.sh
@@ -47,8 +47,8 @@ rm -r __MACOSX
 rm -r Mothur.linux_64.zip
 
 # usearch
-wget -O anaconda2/envs/microbiomeBeef/bin/usearch7.0.1090 $1
-chmod 775 anaconda2/envs/microbiomeBeef/bin/usearch7.0.1090
+wget -O anaconda/envs/microbiomeBeef/bin/usearch7.0.1090 $1
+chmod 775 anaconda/envs/microbiomeBeef/bin/usearch7.0.1090
 
 #sra 
 wget ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz
